f0affd34eb256f71fe948d0c43eda73ff9eb02f9
Moises Osorio <wcoder.mx@gmail.com>
2011-12-14 10:36:58 -0600

    Adds metrics to UI.

--- src/MetricsPanel.py
+++ src/MetricsPanel.py
@@ -3,18 +3,25 @@ Created on Dec 11, 2011
 
 @author: Moises Osorio [WCoder]
 '''
+import math
 
 from PyQt4.QtCore import * #@UnusedWildImport
 from PyQt4.QtGui import * #@UnusedWildImport
 import numpy
-import time
+#import time
 
 from Metrics import Metrics
+
+__EPS__ = 1e-6
 
 class MetricsPanel(QWidget):
     '''
     classdocs
     '''
+    
+    COVERAGE = object()
+    ADDITIVE_EPSILON = object()
+    MULTIPLICATIVE_EPSILON = object()
 
     def __init__(self):
         '''
@@ -31,10 +38,17 @@ class MetricsPanel(QWidget):
         self.table.setColumnCount(0)
         
     def updateMetrics(self, optimalPareto, solutions):
-        self.table.setRowCount(3)
         self.table.setColumnCount(len(solutions))
-        self.table.setHorizontalHeaderLabels([solution[0] for solution in solutions])
-        self.table.setVerticalHeaderLabels(['Error ratio', 'Generational distance', 'Spacing'])
+        solutionNames = [solution[0] for solution in solutions]
+        self.table.setHorizontalHeaderLabels(solutionNames)
+        unary = ['Error ratio', 'Generational distance', 'Spacing']
+        binaryTypes = ['Coverage', 'Additive epsilon', 'Multiplicative epsilon']
+        binary = []
+        for binaryType in binaryTypes:
+            binary += ["%s (%s)" % (binaryType, name) for name in solutionNames]
+        labels = unary + binary
+        self.table.setRowCount(len(labels))
+        self.table.setVerticalHeaderLabels(labels)
         self.table.setAlternatingRowColors(True)
         self.table.setEditTriggers(QTableWidget.NoEditTriggers)
         
@@ -43,31 +57,46 @@ class MetricsPanel(QWidget):
         for column in xrange(len(solutions)):
             values = [[], [], []]
             for solution in solutions[column][1]:
-                tic = time.clock()
+#                tic = time.clock()
                 metrics = Metrics(optimalPareto, solution)
-                print time.clock() - tic
-                tic = time.clock()
+#                print time.clock() - tic
+#                tic = time.clock()
                 values[0].append(metrics.errorRatio())
-                print time.clock() - tic
-                tic = time.clock()
+#                print time.clock() - tic
+#                tic = time.clock()
                 values[1].append(metrics.generationalDistance())
-                print time.clock() - tic
-                tic = time.clock()
+#                print time.clock() - tic
+#                tic = time.clock()
                 values[2].append(metrics.spacing())
-                print time.clock() - tic, "\n\n"
-                tic = time.clock()
+#                print time.clock() - tic, "\n\n"
+#                tic = time.clock()
                 
             for m in xrange(len(values)):
                 mean[m].append(numpy.mean(values[m]))
                 std[m].append(numpy.std(values[m]))
-                
+
+        for metric in [MetricsPanel.COVERAGE, MetricsPanel.ADDITIVE_EPSILON, MetricsPanel.MULTIPLICATIVE_EPSILON]:
+            meanMetric, stdMetric = self._getMetric(solutions, metric)
+            mean += meanMetric
+            std += stdMetric
+            
         for row in xrange(len(mean)):
             for column in xrange(len(mean[row])):
                 m = mean[row][column]
                 s = std[row][column]
+                if m is None or s is None:
+                    continue
+                
                 value = "%.6f / %.6f" % (m, s)
-                if abs(m - min(mean[row])) < 1e-6:
+                if row < len(unary) and abs(m - min(x for x in mean[row] if x is not None)) < __EPS__:
                     value = "<b>%s</b>" % value
+                elif row >= len(unary):
+                    offset = row - (row - len(unary)) % len(solutions)
+                    factor = 1
+                    if row < len(unary) + len(solutions): # Coverage should be maximized
+                        factor = -1
+                    if m*factor < mean[offset + column][row - offset]*factor:
+                        value = "<b>%s</b>" % value
                     
                 self._setMetric(row, column, value)
                 
@@ -75,4 +104,30 @@ class MetricsPanel(QWidget):
         
     def _setMetric(self, row, column, value):
         item = QLabel(value)
+        item.setAlignment(Qt.AlignCenter)
         self.table.setCellWidget(row, column, item)
+
+    def _getMetric(self, solutions, metric):
+        mean = []
+        std = []
+        for i in xrange(len(solutions)):
+            mean.append([0] * len(solutions))
+            std.append([0] * len(solutions))
+            for j in xrange(len(solutions)):
+                if i == j:
+                    mean[i][j] = std[i][j] = None
+                    continue
+                values = []
+                for a in solutions[i][1]:
+                    for b in solutions[j][1]:
+                        metrics = Metrics(None, a, b)
+                        if metric == MetricsPanel.COVERAGE:
+                            values.append(metrics.coverage())
+                        elif metric == MetricsPanel.ADDITIVE_EPSILON:
+                            values.append(metrics.additiveEpsilon())
+                        elif metric == MetricsPanel.MULTIPLICATIVE_EPSILON:
+                            values.append(metrics.multiplicativeEpsilon())
+                mean[i][j] = numpy.mean(values)
+                std[i][j] = numpy.std(values)
+        return mean, std
+    
